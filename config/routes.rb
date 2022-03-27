f591fcf12416e5554a07fb7bc4304157d40fc88e
Matthew O'Donnell <matthew.james.od@gmail.com>
2022-03-27 17:35:06 +0100

    Added admin login functionality

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
   root "posts#most_recent"
   
   resources :posts
+  
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create', as: 'log_in'
+  delete 'logout', to: 'sessions#destroy'
 end
