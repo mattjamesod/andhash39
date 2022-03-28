3b22336dc0c14fb4bd2c03f681970859e3277795
Matthew O'Donnell <matthew.james.od@gmail.com>
2022-03-28 22:41:53 +0100

    minor tweaks

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,11 +5,6 @@ class PostsController < ApplicationController
     @posts = Post.all
   end
   
-  def ribbon
-    @posts = Post.all
-    render "ribbon"
-  end
-  
   def most_recent
     @post = Post.order(:created_at).last
     
