4e941766167bb8f2ec1f644b6d056ad048729e2e
Matthew O'Donnell <matthew.james.od@gmail.com>
2022-04-03 17:00:00 +0100

    Removed show link and added prod host name

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -58,6 +58,8 @@ Rails.application.configure do
 
   # Suppress logger output for asset requests.
   config.assets.quiet = true
+  
+  Rails.application.config.hosts << "andhash39.com"
 
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
